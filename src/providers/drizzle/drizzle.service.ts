5c939060d49e90211dd12f97af8ef155fcea75a0
adilpratama71 <adilpratama71@yahoo.com>
2023-08-21 00:06:04 +0700

    create drizzle only once

--- src/providers/drizzle/drizzle.service.ts
+++ src/providers/drizzle/drizzle.service.ts
@@ -1,18 +1,19 @@
 import { Inject } from '@nestjs/common';
 
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Pool, PoolConfig } from 'pg';
 import { DRIZZLE_OPTIONS } from 'shared/constants';
 
-
 export class DrizzleService {
     private readonly pgPool: Pool;
+    private readonly drizzle: NodePgDatabase;
 
     constructor(@Inject(DRIZZLE_OPTIONS) options: PoolConfig) {
         this.pgPool = new Pool(options);
+        this.drizzle = drizzle(this.pgPool, { logger: true });
     }
 
     get db() {
-        return drizzle(this.pgPool, { logger: true });
+        return this.drizzle;
     }
 }
